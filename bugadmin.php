82db24ef5612f519ab7bdef159f17772ae7dde03
Ron Patterson <ronlpatterson@me.com>
2014-03-06 21:51:12 -0700

    latest updates for 3/6

---
+++ bugadmin.php
@@ -0,0 +1,12 @@
+<?php
+// bugadmin.php
+// Ron Patterson, WildDog Design
+// SQLite version
+#require("btsession.php");
+// connect to the database 
+require("bugcommon.php");
+?>
+<div>
+<input type="button" id="bt_admin_users" value="Users">
+</div>
+<div id="bt_admin_content"></div>
